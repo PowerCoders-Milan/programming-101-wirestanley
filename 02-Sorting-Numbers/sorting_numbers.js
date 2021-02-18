9fa4365cf9ce27840c666899ef31f58cfa7d0e12
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-02-18 14:25:52 +0100

    exercise2

--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -1,6 +1,26 @@
 //insert your pseudocode below
 /*
-here
+create a variable in digit 
+sort the variable in assending order
+print it in this order
 */
 
 //insert your code below
+var num;
+
+function listsGetSortCompare(type, direction) {
+  var compareFuncs = {
+    "NUMERIC": function(a, b) {
+        return Number(a) - Number(b); },
+    "TEXT": function(a, b) {
+        return a.toString() > b.toString() ? 1 : -1; },
+    "IGNORE_CASE": function(a, b) {
+        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+  };
+  var compare = compareFuncs[type];
+  return function(a, b) { return compare(a, b) * direction; }
+}
+
+
+num = [6, 9, 1];
+window.alert(num.slice().sort(listsGetSortCompare("NUMERIC", 1)));
